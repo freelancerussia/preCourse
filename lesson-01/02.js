bb4c893dd35a321dc10dd0ddb9c1dd819b6ba29f
Vlad <voiteh44@gmail.com>
2024-01-04 22:34:52 +0300

    hw3_s1

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,2 +1,9 @@
 let number1 = Math.floor(Math.random() * 100);
 console.log(number1 % 2 === 0 ? `${number1}:Это четное число` : `${number1}:Это нечетное число`);
+
+
+let number = 1;
+console.log(number++);
+console.log(++number);
+console.log(number--);
+console.log(--number);
