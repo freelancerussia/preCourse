9ed81cde78c918d7e706ad96621846ee4a582df5
Vlad <voiteh44@gmail.com>
2024-01-25 22:25:11 +0300

    02-05

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -1,12 +1,12 @@
-let passport = {
-   name: "Petr",
-   surname: "Petrov",
-};
+// let passport = {
+//    name: "Petr",
+//    surname: "Petrov",
+// };
 
-let newPassport = {
-   ...passport,
-   name: "Ivan"
-}
+// let newPassport = {
+//    ...passport,
+//    name: "Ivan"
+// }
 
-console.log(newPassport.name);
-console.log(passport.name);
+// console.log(newPassport.name);
+// console.log(passport.name);
