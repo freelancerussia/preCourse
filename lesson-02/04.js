c3a7dc51e7092cd0ad2785959db70864500b9ff1
Vlad <voiteh44@gmail.com>
2024-01-25 22:31:16 +0300

    02_04

--- lesson-02/04.js
+++ lesson-02/04.js
@@ -1,9 +1,9 @@
 const giveMeRightCondition = (value1, value2, maximum) => {
    if (value1 === value2 && value1 <= maximum) {
-      console.log('Success!');
+      return 'Success!';
    } else {
-      console.log('Something wrong!');
+      return 'Something wrong!';
    }
 }
 
-giveMeRightCondition(100, 100, 20)
+console.log(giveMeRightCondition(100, 100, 20));
