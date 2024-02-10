6973a31b208226dbdd82e762c3810fbc140834ed
Vlad <voiteh44@gmail.com>
2024-02-10 21:32:16 +0300

    02-05

--- lesson-02/05.js
+++ lesson-02/05.js
@@ -1,23 +1,12 @@
-const giveMeRightCondition = (value1, value2, maximum) => {
-   if (value1 === value2 && value1 <= maximum) {
-      console.log('Success!');
-   } else {
-      console.log('Something wrong!');
-   }
+let passport = {
+   name: "Petr",
+   surname: "Petrov",
+};
+
+let newPassport = {
+   ...passport,
+   name: "Ivan"
 }
-giveMeRightCondition(1000, 100, 100)
 
-
-
-// let passport = {
-//    name: "Petr",
-//    surname: "Petrov",
-// };
-
-// let newPassport = {
-//    ...passport,
-//    name: "Ivan"
-// }
-
-// console.log(newPassport.name);
-// console.log(passport.name);
+console.log(newPassport.name);
+console.log(passport.name);
